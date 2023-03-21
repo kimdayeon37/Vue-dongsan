6620be5d390d17e7dad58442be7b405c3ba31868
kimdayeon37 <kdy37912@naver.com>
2023-03-21 18:52:14 +0900

    PWA발행

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pwa: {
+    name: '부동산',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    workboxOptions: {
+      exclude: [/\.map$/, /manifest\.json$/, 'index.html']
+    }
+  }
 })
